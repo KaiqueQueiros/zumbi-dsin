f28ef348534c2f671baaebeda8e907e75fafed2a
Kaique Queiros <kaique_q@outlook.com>
2023-10-17 21:05:29 -0300

    Initial commit

---
+++ src/main/java/com/zumbi/DsinApocalipse/DsinApocalipseApplication.java
@@ -0,0 +1,13 @@
+package com.zumbi.DsinApocalipse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsinApocalipseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsinApocalipseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zumbi/DsinApocalipse/DsinApocalipseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zumbi.DsinApocalipse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsinApocalipseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
